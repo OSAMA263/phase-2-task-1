9def25127d1b9dfe62990c3b9907163140e603bb
osama <osamaelseify2@gmail.com>
2025-03-09 19:47:25 +0200

    added all functnality: swiper,navlinks, accordion,form

---
+++ app.js
@@ -0,0 +1,81 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const navLinks = document.querySelectorAll(".navbar-links a");
+  const sections = document.querySelectorAll("section[id]");
+  const accordionToggler = document.querySelectorAll(".toggle-accordion");
+  const form = document.querySelector("#subscribe-form");
+
+  // active navlink onclick
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      navLinks.forEach((link) => link.classList.remove("active"));
+      link.classList.add("active");
+    });
+  });
+
+  // active link when section inview port
+  function spyActiveLink() {
+    let scrollPosition = window.scrollY + 200;
+
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.offsetHeight;
+      const sectionId = section.getAttribute("id");
+
+      if (
+        scrollPosition >= sectionTop &&
+        scrollPosition < sectionTop + sectionHeight
+      ) {
+        navLinks.forEach((link) => {
+          link.classList.remove("active");
+          if (link.getAttribute("href") === `#${sectionId}`) {
+            link.classList.add("active");
+          }
+        });
+      }
+    });
+  }
+
+  // handle accordion functionality
+  accordionToggler.forEach((toggler) => {
+    toggler.addEventListener("click", () => {
+      const content = toggler.nextElementSibling;
+      toggler.classList.toggle("active");
+      if (toggler.classList.contains("active")) {
+        content.style.height = content.scrollHeight + "px";
+      } else {
+        content.style.height = 0;
+      }
+    });
+  });
+
+  // swiper slides review
+  new Swiper(".swiper", {
+    loop: true,
+    slidesPerView: 2,
+    spaceBetween: 40,
+    navigation: {
+      nextEl: "#next-slide",
+      prevEl: "#prev-slide",
+    },
+  });
+
+  // handle form submission
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const email = form.querySelector("input[type='email']").value;
+    const popup = document.querySelector("#popup");
+    popup.innerHTML = `
+      <h2>Thank you for joining us</h2>
+      <p>${email}</p>
+    `;
+    popup.style.transform = "translate(-50%,50%)";
+
+    setTimeout(() => {
+      popup.style.transform = "translate(-50%,-200%)";
+    }, 3000);
+
+    form.reset();
+  });
+
+  window.addEventListener("scroll", spyActiveLink);
+});
